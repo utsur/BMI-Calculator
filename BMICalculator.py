3536151b3ded9a2ca2d0c5829e80524776cf92fd
utsur <utsur@student.kit.edu>
2025-04-10 22:13:23 +0200

    first commit

---
+++ BMICalculator.py
@@ -0,0 +1,7 @@
+# Simple BMI Calculator
+
+mass = float(input('Enter your wight (in kg): ')) # weight in kg
+height = float(input('Enter your wight (in m): ')) # height in m
+
+bmi = mass / height ** 2
+print(bmi)
