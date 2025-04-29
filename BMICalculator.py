6450ac22342e008106c0bb30cac2666fd0d302d5
utsur <utsur@student.kit.edu>
2025-04-29 10:01:38 +0200

    Removed typing errors

--- BMICalculator.py
+++ BMICalculator.py
@@ -1,7 +1,7 @@
 # Simple BMI Calculator
 
-mass = float(input('Enter your wight (in kg): ')) # weight in kg
-height = float(input('Enter your wight (in m): ')) # height in m
+mass = float(input('Enter your weight (in kg): ')) # weight in kg
+height = float(input('Enter your height (in m): ')) # height in m
 
 bmi = mass / height ** 2
 print(bmi)
